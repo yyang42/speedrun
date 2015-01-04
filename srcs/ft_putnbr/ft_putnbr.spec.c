708f5c0b1992f007058899064619ef5e1144f948
yyang <yan.yaang@gmail.com>
2015-01-04 21:44:19 +0100

    add putnbr

---
+++ srcs/ft_putnbr/ft_putnbr.spec.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h>
+#include <strings.h>
+
+void	ft_putnbr(int n);
+
+int		main(void)
+{
+	ft_putnbr(123456789);
+	printf("\nCheck result visually, should be equal to '123456789'\n");
+	return (0);
+}
